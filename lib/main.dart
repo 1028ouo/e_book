ecb8263b6dc71cccb4cf3647c6445b98540c6b9b
1028ouo <shellyyi1028@gmail.com>
2025-04-06 16:00:49 +0800

    新增音樂背景

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'services/audio_service.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AudioService _audioService = AudioService();
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeAudio();
+  }
+
+  Future<void> _initializeAudio() async {
+    await _audioService.initialize();
+    await _audioService.play();
+  }
+
+  @override
+  void dispose() {
+    _audioService.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/services/audio_service.dart
@@ -0,0 +1,41 @@
+import 'package:just_audio/just_audio.dart';
+
+class AudioService {
+  static final AudioService _instance = AudioService._internal();
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  bool _isInitialized = false;
+
+  factory AudioService() {
+    return _instance;
+  }
+
+  AudioService._internal();
+
+  Future<void> initialize() async {
+    if (!_isInitialized) {
+      // 請確保在assets資料夾中有background_music.mp3檔案
+      await _audioPlayer.setAsset('assets/magic_forest.mp3');
+      await _audioPlayer.setLoopMode(LoopMode.all);
+      _isInitialized = true;
+    }
+  }
+
+  Future<void> play() async {
+    if (!_isInitialized) {
+      await initialize();
+    }
+    await _audioPlayer.play();
+  }
+
+  Future<void> pause() async {
+    await _audioPlayer.pause();
+  }
+
+  Future<void> stop() async {
+    await _audioPlayer.stop();
+  }
+
+  void dispose() {
+    _audioPlayer.dispose();
+  }
+}
